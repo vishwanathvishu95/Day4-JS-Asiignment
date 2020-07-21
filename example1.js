b73e28d69982c5324c4fd89b6b26e101266a5ed9
vishwanathvishu95 <68235923+vishwanathvishu95@users.noreply.github.com>
2020-07-21 19:30:46 +0530

    Assignment5

---
+++ example1.js
@@ -0,0 +1,11 @@
+while(!false){
+    let num=prompt('Enter the number');
+    if(num<101){
+        console.log('number:'+num);
+        break;
+    }
+    else{
+        console.log('number:'+num);
+        continue;
+    }
+}
